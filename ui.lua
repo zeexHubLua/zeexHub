ed756885338539fa249b1b34b29db2f4a8bce40f
zeexHubLua <lip88225255@gmail.com>
2026-03-17 17:21:48 +0300

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -6,6 +6,12 @@ local RunService = game:GetService("RunService")
 local player = Players.LocalPlayer
 local mouse = player:GetMouse()
 
+-- ЗАГРУЗКА МАКРО СИСТЕМЫ
+local MacroSystem = loadstring(game:HttpGet("https://raw.githubusercontent.com/zeexHubLua/zeexHub/refs/heads/main/macroLogic.lua"))()
+local macroLogic = setmetatable({}, MacroSystem)
+
+print("✅ Макро система загружена")
+
 -- ==========================================
 -- ЦВЕТА
 -- ==========================================
